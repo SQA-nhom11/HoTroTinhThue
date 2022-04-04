72751053f401b48d7f8e0b2154da947948d89639
manhcuong3092 <cuongdm3092@gmail.com>
2022-04-04 16:52:30 +0700

    Set up base project

---
+++ src/main/java/com/example/hotrotinhthue/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.example.hotrotinhthue.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class IndexController {
+    @GetMapping({"index", "/"})
+    public String index(Model model) {
+        return "index";
+    }
+
+}
