410029c959ad16af6235bb68bd54da0eb74bd98a
manhcuong3092 <cuongdm3092@gmail.com>
2022-04-04 16:31:39 +0700

    Initial commit

---
+++ src/main/java/com/example/hotrotinhthue/HoTroTinhThueApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotrotinhthue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoTroTinhThueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoTroTinhThueApplication.class, args);
+    }
+
+}
